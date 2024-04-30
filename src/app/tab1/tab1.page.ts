440c786fb6cc4197ea0e33e507b97e1ed54d2712
ceda-dev-01 <l.lezzerini@coopceda.it>
2024-04-30 15:54:53 +0200

    Added alert, buttons and cards

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, AlertInput} from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,65 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public alertButtons = [
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      handler: () => {
+        console.log('Alert canceled');
+      },
+    },
+    {
+      text: 'OK',
+      role: 'confirm',
+      handler: () => {
+        console.log('Alert confirmed');
+      },
+    },
+  ];
 
+  public alertInputs: AlertInput[] = [
+    {
+      placeholder: 'Name',
+    },
+    {
+      placeholder: 'Nickname (max 8 characters)',
+      attributes: {
+        maxlength: 8,
+      },
+    },
+    {
+      type: 'number',
+      placeholder: 'Age',
+      min: 1,
+      max: 100,
+    },
+    {
+      type: 'textarea',
+      placeholder: 'A little about yourself',
+    },
+  ];
+
+  constructor(private alertController: AlertController) { }
+
+  async presentAlert() {
+    console.log("I'm here!");
+    const alert = await this.alertController.create({
+      header: 'A Short Title Is Best',
+      subHeader: 'A Sub Header Is Optional',
+      message: 'A message should be a short, complete sentence.',
+      buttons: this.alertButtons,
+      inputs: this.alertInputs,
+    });
+
+    await alert.present();
+  }
+
+  blur01(){
+    console.log("Blurred!");
+  }
+
+  focus01(){
+    console.log("Focused!");
+  }
 }
